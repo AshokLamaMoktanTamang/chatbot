789b2dde76e6f6ae0e01e405a8ccd29e4c2a0a50
Ashok Lama <ashok.lama@innovatetech.co>
2023-10-14 22:13:25 +0545

    [Add] Added the react router component

--- apps/chatbot-webapp/src/app/app.tsx
+++ apps/chatbot-webapp/src/app/app.tsx
@@ -1,13 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+
 import { CssBaseline, ThemeProvider } from "@mui/material";
 
 import { theme } from "../assets/theme";
 import Layout from "../layout";
+import ChatBox from "../components/ChatBox";
 
 export function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Layout />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="" element={<ChatBox />} />
+        </Route>
+      </Routes>
     </ThemeProvider>
   );
 }
--- apps/chatbot-webapp/src/main.tsx
+++ apps/chatbot-webapp/src/main.tsx
@@ -2,12 +2,17 @@ import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 
 import App from './app/app';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
